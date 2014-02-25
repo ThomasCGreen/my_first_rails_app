cf2580685c2f4e6c96f3b13e1b06a49c89035904
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-02-25 19:05:08 +0000

    Added new functionality to my_first_rails_app:
    * /time/now provides the current time
    
    * /introduce/name1/and/name2 provides an introduction between two people
    
    [#66430074]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MyFirstRailsApp::Application.routes.draw do
-  get 'hello'=> "welcome#hello"
-  get 'hello/:name'=> 'welcome#hello'
+  get 'hello' => "welcome#hello"
+  get 'hello/:name' => "welcome#hello"
   get 'time/now' => "time#now"
+  get 'introduce/:first_person/and/:second_person' => "introduce#people"
 end
